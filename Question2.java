9ae1fa28b095290e669b3c420bdee9331918023c
Huy Minh Tran <s10223485@connect.np.edu.sg>
2022-04-23 23:44:28 +0800

    Completed Q2

--- Question2.java
+++ Question2.java
@@ -11,6 +11,12 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.println("Enter Height(m): ");
     double height = in.nextDouble();
+    System.out.println("Enter Weight(kg): ");
+    double weight = in.nextDouble();
+    double bmi = weight / (height*height);
+    
+    System.out.println("BMI: " + bmi);
   }
 }
