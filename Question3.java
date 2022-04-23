fdfd3928029dd8935b497c7cd6542382055f875a
Huy Minh Tran <s10223485@connect.np.edu.sg>
2022-04-24 00:26:56 +0800

    Removed System.out.print("Enter an integer"); from Q3

--- Question3.java
+++ Question3.java
@@ -11,7 +11,6 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    System.out.print("Enter an integer");
     int num = in.nextInt();
     int multiple = num*num;
 
